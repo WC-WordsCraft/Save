2572bacf04a255d20c8efd857aaee847721b9020
上杉夏相 <ssxx.site@aliyun.com>
2021-03-14 10:25:03 +0800

    规范格式化代码

--- Save.py
+++ Save.py
@@ -11,6 +11,7 @@ PLUGIN_METADATA = {
     'link': 'https://github.com/A-Words/Save',
 }
 
+
 # 插件加载、重载、卸载事件
 def on_load(server, prev_module):
     if prev_module is None:
@@ -22,30 +23,33 @@ def on_load(server, prev_module):
     server.register_help_message('!!save', 'Save the worlds or plugins')
     server.register_command(
         Literal('!!save'). \
-        then(
+            then(
             Literal('all').runs(save_all)
         ). \
-        then(
+            then(
             Literal('world'). \
-            then(Text('name'))
+                then(Text('name'))
         ). \
-        then(
+            then(
             Literal('plugin'). \
-            then(Text('name'))
+                then(Text('name'))
         )
     )
 
-#插件移除
+
+# 插件移除
 def on_remove(server):
     server.logger.info('插件停止使用')
 
-#发送信息
-def print_message(source: CommandSource, msg, tell = True, prefix = '[Save]'):
+
+# 发送信息
+def print_message(source: CommandSource, msg, tell=True, prefix='[Save]'):
     msg = prefix + msg
     if source.is_player and not tell:
         source.get_server().say(msg)
     else:
         source.reply(msg)
+
 
 # 保存函数
 def save_all(source: CommandSource):
